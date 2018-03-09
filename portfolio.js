6f55592f08d983c2f1b0b90136408fabd961e357
Jabbrowocky <mindbull3tz@gmail.com>
2018-03-08 19:46:37 -0600

    created two sample cards with images, organized file directory, cleaned up css, including adding two more font face rules

--- portfolio.js
+++ portfolio.js
@@ -1,16 +1,8 @@
 
 
-function toggleOverlay(){
-    
-    
-    document.getElementById("overlay").style.opacity = "0";
-    document.getElementById("overlay").classList.toggle("slideUp");
-}
-
 $(document).ready(function(){
     $("#overlayButton").click(function(){
-        $("#overlay").slideUp()
-                       
+        $("#overlay").slideUp()         
     
     });
 });
